13d1c134ef2f8f619a7c83852fdfb7ecfa4bd36d
NoClay <im.noclay@gmail.com>
2017-05-13 17:34:59 +0800

    修改了ReadMe

---
+++ app/src/main/java/wang/fly/com/yunhealth/MyViewPackage/Dialogs/InputTimeAndDoseDialog.java
@@ -0,0 +1,10 @@
+package wang.fly.com.yunhealth.MyViewPackage.Dialogs;
+
+import android.widget.PopupWindow;
+
+/**
+ * Created by noclay on 2017/5/13.
+ */
+
+public class InputTimeAndDoseDialog extends PopupWindow {
+}
