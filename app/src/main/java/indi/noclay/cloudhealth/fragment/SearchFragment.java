c12da085ea1abcae512061f003e8db657fe88294
NoClay <im.noclay@gmail.com>
2018-05-02 00:44:36 +0800

    添加百问页面UI

--- app/src/main/java/indi/noclay/cloudhealth/fragment/SearchFragment.java
+++ app/src/main/java/indi/noclay/cloudhealth/fragment/SearchFragment.java
@@ -3,11 +3,15 @@ package indi.noclay.cloudhealth.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.PopupWindow;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import indi.noclay.cloudhealth.R;
 
@@ -19,21 +23,33 @@ import indi.noclay.cloudhealth.R;
 public class SearchFragment extends Fragment {
 
     View mView;
+    private RelativeLayout searchBar;
+    private EditText searchInput;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_search, container, false);
-        initWindowInputMode();
+        initView();
         return mView;
     }
 
 
-
-    private void initWindowInputMode() {
-        this.getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
-        this.getActivity().getWindow().setSoftInputMode(PopupWindow.INPUT_METHOD_NEEDED);
-        this.getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+    private void initView() {
+        searchBar = (RelativeLayout) mView.findViewById(R.id.search_bar);
+        searchInput = (EditText) mView.findViewById(R.id.search_input);
+        searchInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                switch (actionId){
+                    case EditorInfo.IME_ACTION_SEARCH:{
+                        Toast.makeText(getContext(), "搜索" + searchInput.getText().toString(), Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
     }
 }
